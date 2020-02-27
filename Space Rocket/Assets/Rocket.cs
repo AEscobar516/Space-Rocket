d53066333e1b8c5106f20320ac0da0c15d8ab874
AEscobar516 <55443534+AEscobar516@users.noreply.github.com>
2020-02-27 08:42:27 -0500

    asteroids move now

--- Space Rocket/Assets/Rocket.cs
+++ Space Rocket/Assets/Rocket.cs
@@ -6,7 +6,7 @@ public class Rocket : MonoBehaviour
 {
     public Vector3[] rocketPos;
     public int currPosIndex = 1;
-    public float moveSpeed = 8;
+    public float moveSpeed = 7;
     public float t = 0;
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,24 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime * moveSpeed;
-        transform.position = Vector3.Lerp(transform.position, rocketPos[currPosIndex], t);  
+        t = Time.deltaTime * moveSpeed;
+        transform.position = Vector3.Lerp(transform.position, rocketPos[currPosIndex], t);
+
+        if (Input.GetKeyDown(KeyCode.W)) 
+        {
+            currPosIndex = currPosIndex + 1;
+                if (currPosIndex > rocketPos.Length - 1) 
+            {
+                currPosIndex = rocketPos.Length - 1;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            currPosIndex = currPosIndex - 1;
+            if(currPosIndex < 0)
+            {
+                currPosIndex = 0;
+            }
+        }
     }
 }
